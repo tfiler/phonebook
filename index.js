e7f025bf735eafc747eed68a334032fe4b6470e7
Trevor <trevorfiler03@gmail.com>
2022-04-28 20:36:24 -0400

    Added POST and DELETE

--- index.js
+++ index.js
@@ -31,8 +31,70 @@ app.get('/', (request, response) => {
   response.send('work in progress')
 })
 
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>
+      Phonebook has info for ${persons.length} people
+    </p>
+    <p>
+      ${new Date()}
+    </p>
+  `)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id',  (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = 1000000
+
+  return Math.round(Math.random()*maxId)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  } else if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  } else if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name already exists'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3000
